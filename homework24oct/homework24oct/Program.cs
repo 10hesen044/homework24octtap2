b1e84d8b8c3e73dcca417fcbdc24fc6d9bb5d603
10hesen044 <tu201906250@code.edu.az>
2021-10-25 02:16:32 +0400

    project added

---
+++ homework24oct/homework24oct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework24oct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int say = 0;
+            for (int i = 0; i < 8000; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    say++;
+                }
+                i++;
+            }
+            Console.WriteLine("1-den 8000-dek ededler icerisinde cut olanlarin sayi:" + say);
+        }
+    }
+}
